5e033905d0e67e83fb76fae45259b234dbf8a573
Job Noorman <job@noorman.info>
2017-08-31 20:55:53 +0200

    Implement integral_constant
    As well as the helpers bool_constant, true_type, and false_type.

--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -6,6 +6,24 @@
 namespace STDAVR_NAMESPACE
 {
 
+template<class T, T V>
+struct integral_constant
+{
+    using value_type = T;
+    using type = integral_constant;
+
+    static constexpr T value = V;
+
+    constexpr operator value_type() const noexcept   {return value;}
+    constexpr value_type operator()() const noexcept {return value;}
+};
+
+template<bool B>
+using bool_constant = integral_constant<bool, B>;
+
+using true_type  = bool_constant<true>;
+using false_type = bool_constant<false>;
+
 template<typename T> struct remove_reference      {using type = T;};
 template<typename T> struct remove_reference<T&>  {using type = T;};
 template<typename T> struct remove_reference<T&&> {using type = T;};
--- test/type_traits_test.cpp
+++ test/type_traits_test.cpp
@@ -10,6 +10,10 @@ namespace
 using some_type = int;
 using some_type2 = double;
 
+using some_integral_type = unsigned long;
+constexpr some_integral_type some_integral_value = 4042;
+constexpr bool some_bool_value = true;
+
 }
 
 TEST(remove_reference, keeps_non_reference_type_intact)
@@ -50,4 +54,67 @@ TEST(void_t, is_void_given_any_number_of_type_arguments)
     StaticAssertTypeEq<sut::void_t<>, void>();
     StaticAssertTypeEq<sut::void_t<some_type>, void>();
     StaticAssertTypeEq<sut::void_t<some_type, some_type2>, void>();
+}
+
+TEST(integral_constant, has_the_given_type_as_value_type_member)
+{
+    using ic = sut::integral_constant<some_integral_type, some_integral_value>;
+
+    StaticAssertTypeEq<ic::value_type, some_integral_type>();
+    StaticAssertTypeEq<ic::type, ic>();
+}
+
+TEST(integral_constant, has_ist_own_type_as_type_member)
+{
+    using ic = sut::integral_constant<some_integral_type, some_integral_value>;
+
+    StaticAssertTypeEq<ic::type, ic>();
+}
+
+TEST(integral_constant, has_the_given_value_as_value_member)
+{
+    using ic = sut::integral_constant<some_integral_type, some_integral_value>;
+
+    ASSERT_THAT(ic::value, Eq(some_integral_value));
+}
+
+TEST(integral_constant, implicitly_converts_to_the_given_value)
+{
+    auto ic = sut::integral_constant<some_integral_type, some_integral_value>{};
+
+    some_integral_type value = ic;
+
+    ASSERT_THAT(value, Eq(some_integral_value));
+}
+
+TEST(integral_constant, returns_the_given_value_when_called)
+{
+    auto ic = sut::integral_constant<some_integral_type, some_integral_value>{};
+
+    some_integral_type value = ic();
+
+    ASSERT_THAT(value, Eq(some_integral_value));
+}
+
+TEST(bool_constant, is_an_integral_constant_with_type_bool)
+{
+    using bc = sut::bool_constant<some_bool_value>;
+
+    StaticAssertTypeEq<bc::value_type, bool>();
+}
+
+TEST(true_type, has_value_true)
+{
+    using tt = sut::true_type;
+
+    StaticAssertTypeEq<tt::value_type, bool>();
+    ASSERT_TRUE(tt::value);
+}
+
+TEST(false_type, has_value_false)
+{
+    using ft = sut::false_type;
+
+    StaticAssertTypeEq<ft::value_type, bool>();
+    ASSERT_FALSE(ft::value);
 }
